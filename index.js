69e7713d56ff4566038b37629a278ec319324c90
Trevor Dunlap <tjdunlap1899@gmail.com>
2023-01-10 14:52:52 -0500

    commit tree 0d2dacab8d65d936bc46e0f6b6f39dc1e967689f

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam", 
+    streetAddress: "2001 Jupiter Street",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+    };
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
+
